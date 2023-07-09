b34cfc51c62d0731bea82be529e18baa5731fac7
FranMF8 <cejita678@gmail.com>
2023-07-09 19:17:12 -0300

    Added final commit

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
-let url = '';
-
+let url = 'https://d940-181-231-122-56.ngrok-free.app/student';
 
 window.onload = function() {
-    
-}
+    getStudents();
+};
 
-function loadStudents(){
+function loadStudents() {
     return new Promise(function(resolve, reject) {
         var request = new XMLHttpRequest();
-        request.open('GET', url + '/getAll')
-        request.responseType = 'json'
-
-        request.onload = function(){
-            if(request.status == 200)  resolve(request.response)
-            else reject(Error(request.statusText))
-        }
-    })
+        request.open('GET', url + '/getAll');
+        request.responseType = 'json';
+        request.onload = function() {
+            if (request.status == 200) {
+                resolve(request.response);
+            } else {
+                reject(Error(request.statusText));
+            }
+        };
+        request.onerror = function() {
+            reject(Error('Error: unexpected network error.'));
+        };
+        request.send();
+    });
 }
 
 function getStudents() {
-    loadStudents().then( response => {
-        var output = document.getElementById('output-container')
-        response.forEach( element => {
-            var row = output.insertRow()
+    loadStudents().then(response => {
+        var output = document.getElementById('tableBody');
+        output.innerHTML = "";
+        response.forEach(element => {
+            var row = output.insertRow();
 
-            var id = row.insertCell()
-            id.innerHTML = element.id
+            var id = row.insertCell();
+            id.innerHTML = element.id;
 
-            var dni = row.insertCell()
-            dni.innerHTML = element.dni
+            var dni = row.insertCell();
+            dni.innerHTML = element.dni;
 
-            var lastName = row.insertCell()
-            lastName.innerHTML = element.lastName
-            
-            var firstName = row.insertCell()
-            firstName.innerHTML = element.firstName
+            var lastName = row.insertCell();
+            lastName.innerHTML = element.lastName;
 
-            var email = row.insertCell()
-            email.innerHTML = element.email
+            var firstName = row.insertCell();
+            firstName.innerHTML = element.firstName;
+
+            var email = row.insertCell();
+            email.innerHTML = element.email;
 
             var student = JSON.stringify({
                 'id': element.id,
@@ -45,15 +51,9 @@ function getStudents() {
                 'lastName': element.lastName,
                 'firstName': element.firstName,
                 'email': element.email
-            })
-            var show = row.insertCell()
-            show.innerHTML = `<button onclick='showStudents(${student})'>Show Students</button>`
-        })
+            });
+        });
+        console.log("Estudiantes actualizados")
     })
-    .catch(/* reason => {
-        console.error(caca)
-    }*/
-    console.error("Error")
-    )
+    .catch(error => console.error("Error"));
 }
-
